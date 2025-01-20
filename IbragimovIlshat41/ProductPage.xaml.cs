c4510fb9056f7664e405e9e95f297b3b703e458c
Ilshat Ibragimov <ibragimoff.ilshat2006@gmail.com>
2025-01-20 15:48:07 +0500

    18 лаба

--- IbragimovIlshat41/ProductPage.xaml.cs
+++ IbragimovIlshat41/ProductPage.xaml.cs
@@ -20,18 +20,82 @@ namespace IbragimovIlshat41
     /// </summary>
     public partial class ProductPage : Page
     {
+        List<Product> Tablelist;
+        int RecordsCount;
         public ProductPage()
         {
             InitializeComponent();
 
             var currentAgents = IbragimovIlshat41Entities.GetContext().Product.ToList();
+            Tablelist = IbragimovIlshat41Entities.GetContext().Product.ToList();
 
             ProductListView.ItemsSource = currentAgents;
+
+            ComboType.SelectedIndex = 0;
+
+            UpdateProducts();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void UpdateProducts()
+        {
+            var currentProducts = IbragimovIlshat41Entities.GetContext().Product.ToList();
+
+            if (ComboType.SelectedIndex == 0)
+                currentProducts = currentProducts.Where(p => p.ProductDiscountAmount >= 0 && p.ProductDiscountAmount <= 100).ToList();
+
+            if (ComboType.SelectedIndex == 1)
+                currentProducts = currentProducts.Where(p => p.ProductDiscountAmount >= 0 && p.ProductDiscountAmount < 10).ToList();
+
+            if (ComboType.SelectedIndex == 2)
+                currentProducts = currentProducts.Where(p => p.ProductDiscountAmount >= 10 && p.ProductDiscountAmount < 15).ToList();
+
+            if (ComboType.SelectedIndex == 3)
+                currentProducts = currentProducts.Where(p => p.ProductDiscountAmount >= 15 && p.ProductDiscountAmount <= 100).ToList();
+
+
+            currentProducts = currentProducts.Where(p => p.ProductName.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+
+
+            if (RBdown.IsChecked.Value)
+            {
+                currentProducts = currentProducts.OrderByDescending(p => p.ProductCost).ToList();
+            }
+
+            if (RBup.IsChecked.Value)
+            {
+                currentProducts = currentProducts.OrderBy(p => p.ProductCost).ToList();
+            }
+
+            ProductListView.ItemsSource = currentProducts.ToList();
+
+            TBCount.Text = "кол-во " + currentProducts.Count.ToString();
+            TBAllRecords.Text = "из " + Tablelist.Count.ToString();
+        }
+
+        private void RBup_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateProducts();
+        }
+
+
+        private void RBdown_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateProducts();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateProducts();
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateProducts();
         }
     }
 }
